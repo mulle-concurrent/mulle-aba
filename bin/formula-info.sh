32cc4a2f58449bffd059891394c71fb7d3f58618
Nat! <nat@mulle-kybernetik.com>
2018-02-19 23:49:51 +0100

    remove .bootstrap, remove bin, add mulle-project, remove templates

--- bin/formula-info.sh
+++
@@ -1,5 +0,0 @@
-PROJECT="mulle-aba"   # your project name, requires camel-case
-DESC="A portable, lock-free solution to the ABA problem in C"
-DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11
-${DEPENDENCY_TAP}mulle-allocator
-${DEPENDENCY_TAP}mulle-thread'
