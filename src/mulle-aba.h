6cf229cf1738e1da88d641a251d387487bc201c4
Nat! <nat@mulle-kybernetik.com>
2018-05-27 15:07:05 +0200

    fix missing eval

--- src/mulle-aba.h
+++ src/mulle-aba.h
@@ -38,7 +38,7 @@
 //
 // community version is always even
 //
-#define MULLE_ABA_VERSION     ((2 << 20) | (0 << 8) | 1)
+#define MULLE_ABA_VERSION     ((2 << 20) | (0 << 8) | 2)
 
 #include "dependencies.h"
 
