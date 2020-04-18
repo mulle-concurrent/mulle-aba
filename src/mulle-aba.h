de94d15cc10afb87d45870ccda04ecb0f783d99e
Nat! <nat@mulle-kybernetik.com>
2020-04-18 20:13:00 +0200

    * new mulle-sde project structure

--- src/mulle-aba.h
+++ src/mulle-aba.h
@@ -38,7 +38,7 @@
 //
 // community version is always even
 //
-#define MULLE_ABA_VERSION     ((2 << 20) | (0 << 8) | 18)
+#define MULLE_ABA_VERSION     ((2 << 20) | (0 << 8) | 19)
 
 #include "include.h"
 
