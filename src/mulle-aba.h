3d46f2664fb346ce188fa0e85414b3df9d86452f
Nat! <nat@mulle-kybernetik.com>
2026-01-13 22:32:06 +0100

    maintenance: upgrade mulle-c tooling

--- src/mulle-aba.h
+++ src/mulle-aba.h
@@ -38,7 +38,7 @@
 //
 // community version is always even
 //
-#define MULLE__ABA_VERSION     ((3UL << 20) | (1 << 8) | 2)
+#define MULLE__ABA_VERSION     ((3UL << 20) | (1 << 8) | 3)
 
 #include "include.h"
 
--- src/reflect/_mulle-aba-versioncheck.h
+++ src/reflect/_mulle-aba-versioncheck.h
@@ -7,7 +7,7 @@
 
 #if defined( MULLE__ALLOCATOR_VERSION)
 # ifndef MULLE__ALLOCATOR_VERSION_MIN
-#  define MULLE__ALLOCATOR_VERSION_MIN  ((7UL << 20) | (1 << 8) | 0)
+#  define MULLE__ALLOCATOR_VERSION_MIN  ((7UL << 20) | (1 << 8) | 1)
 # endif
 # ifndef MULLE__ALLOCATOR_VERSION_MAX
 #  define MULLE__ALLOCATOR_VERSION_MAX  ((8UL << 20) | (0 << 8) | 0)
@@ -25,7 +25,7 @@
 #endif
 #if defined( MULLE__LINKEDLIST_VERSION)
 # ifndef MULLE__LINKEDLIST_VERSION_MIN
-#  define MULLE__LINKEDLIST_VERSION_MIN  ((0UL << 20) | (0 << 8) | 5)
+#  define MULLE__LINKEDLIST_VERSION_MIN  ((0UL << 20) | (0 << 8) | 6)
 # endif
 # ifndef MULLE__LINKEDLIST_VERSION_MAX
 #  define MULLE__LINKEDLIST_VERSION_MAX  ((0UL << 20) | (1 << 8) | 0)
@@ -43,7 +43,7 @@
 #endif
 #if defined( MULLE__THREAD_VERSION)
 # ifndef MULLE__THREAD_VERSION_MIN
-#  define MULLE__THREAD_VERSION_MIN  ((4UL << 20) | (6 << 8) | 2)
+#  define MULLE__THREAD_VERSION_MIN  ((4UL << 20) | (7 << 8) | 0)
 # endif
 # ifndef MULLE__THREAD_VERSION_MAX
 #  define MULLE__THREAD_VERSION_MAX  ((5UL << 20) | (0 << 8) | 0)
